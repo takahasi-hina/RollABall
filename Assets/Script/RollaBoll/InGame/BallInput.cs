d8a078fff88446962fd13be37b055defb13542c1
takahasi-hina <134469679+takahasi-hina@users.noreply.github.com>
2023-11-15 15:48:24 +0900

    kousinn

--- Assets/Script/RollaBoll/InGame/BallInput.cs
+++ Assets/Script/RollaBoll/InGame/BallInput.cs
@@ -3,23 +3,31 @@ using UnityEngine.InputSystem;
 
 public class BallInput : MonoBehaviour
 {
+    public GameObject Ball;
     // Update is called once per frame
     void Update()
     {
         // 新しいInput Systemを使用して"W"キーの入力を検知
         if (Keyboard.current.wKey.wasPressedThisFrame)
         {
-            Debug.Log("Wキーが押されました!");
+            Ball.transform.position += Vector3.forward;
+            
         }
         // Wキーが押され続けている間
-        if (Keyboard.current.wKey.isPressed)
+        if (Keyboard.current.aKey.isPressed)
         {
-            Debug.Log("Wキーが押され続けてます!");
+            Ball.transform.position += Vector3.left;
         }
         // Wキーが離された瞬間だけ評価する
-        if (Keyboard.current.wKey.wasReleasedThisFrame)
+        if (Keyboard.current.sKey.wasReleasedThisFrame)
         {
-            Debug.Log("Wキーが離されました!");
+            Ball.transform.position += Vector3.back;
+            
+        }
+        if (Keyboard.current.dKey.wasReleasedThisFrame)
+        {
+            Ball.transform.position += Vector3.right;
+
         }
     }
 }
